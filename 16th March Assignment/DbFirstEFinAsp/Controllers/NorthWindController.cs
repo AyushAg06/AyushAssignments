5f17abd8813a6be3d53f33939f77d5a06cca76d8
AyushAg06 <ayushagarwala200@gmail.com>
2026-03-16 16:12:21 +0530

    16th march commit

---
+++ 16th March Assignment/DbFirstEFinAsp/Controllers/NorthWindController.cs
@@ -0,0 +1,58 @@
+﻿using DbFirstEFinAsp.Models;
+using DbFirstEFinAsp.Models.NorthWindViewModels;
+using Microsoft.AspNetCore.Mvc;
+namespace DbFirstEFinAsp.Controllers
+{
+    public class NorthWindController : Controller
+    {
+        public IActionResult SpainCustomer()
+        {
+            NorthWindContext context = new NorthWindContext();
+            var spainCustomers = context.Customers.Where(x => x.Country == "Spain").Select(x => new SpainCustomerViewModel
+            {
+                Cid = x.CustomerId,
+                Cname = x.ContactName,
+                Comname = x.CompanyName
+            }).ToList();
+
+            return View(spainCustomers);
+        }
+        public IActionResult SearchCustomer(string ContactName)
+        {
+            NorthWindContext context = new NorthWindContext();
+            var SearchCustomers= context.Customers.Where(x => x.ContactName == ContactName).Select(x => new SpainCustomerViewModel
+            {
+                Cid = x.CustomerId,
+                Cname = x.ContactName,
+                Comname = x.CompanyName
+            }).ToList();
+            return View(SearchCustomers);
+        }
+        [HttpGet]
+        public IActionResult EditCustomer(string CustomerId)
+        {
+            NorthWindContext context = new NorthWindContext();
+            var EditCustomers = context.Customers.Where(x => x.CustomerId==CustomerId).Select(x => new SpainCustomerViewModel
+            {
+                Cid = x.CustomerId,
+                Cname = x.ContactName,
+                Comname = x.CompanyName
+            }).FirstOrDefault();
+
+            return View(EditCustomers);
+        }
+        [HttpPost]
+        public IActionResult EditCustomer(SpainCustomerViewModel model)
+        {
+            NorthWindContext context = new NorthWindContext();
+            var Customer=context.Customers.FirstOrDefault(x=>x.CustomerId==model.Cid);
+            if(Customer!=null)
+            {
+                Customer.CompanyName = model.Cname;
+                Customer.CompanyName = model.Comname;
+                context.SaveChanges();
+            }
+            return RedirectToAction("SpainCustomer");
+        }
+    }
+}
---
+++ 16th March Assignment/DbFirstEFinAsp/Models/NorthWindViewModels/SpainCustomerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DbFirstEFinAsp.Models.NorthWindViewModels
+{
+    public class SpainCustomerViewModel
+    {
+        [Key]
+            public string Cid { get; set; }
+            public string Cname { get; set; }
+            public string Comname { get; set; }
+        
+    }
+}
